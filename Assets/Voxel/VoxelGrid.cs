0c39ea2485fe1051dd78a68dbf1ae5831bc1a516
Bruhtek <bartosz.kas2005@gmail.com>
2023-01-24 19:42:57 +0100

    Add colors to vectors depending on their fill level

--- Assets/Voxel/VoxelGrid.cs
+++ Assets/Voxel/VoxelGrid.cs
@@ -4,7 +4,11 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class VoxelGrid : MonoBehaviour {
-	public int resolution = 10;
+	public int resolution = 16;
+	public float threshold = 0.5f;
+
+	// 0-1 range, 0 is empty, 1 is full
+	public float[][][] voxels;
 	
 	[SerializeField]
 	private Mesh _mesh;
@@ -13,6 +17,9 @@ public class VoxelGrid : MonoBehaviour {
 
 	[SerializeField, Range(0.01f, 0.3f)]
 	private float scale = 0.1f;
+	
+	[SerializeField]
+	private Gradient _gradient = new Gradient();
 	
 	private ComputeBuffer _matricesBuffer;
 	private Matrix4x4[] _matrices;
@@ -26,10 +33,13 @@ public class VoxelGrid : MonoBehaviour {
 		_matricesBuffer = new ComputeBuffer(length, stride);
 		_matrices = new Matrix4x4[length];
 		
+		voxels = new float[resolution][][];
 		for (int x = 0; x < resolution; x++) {
+			voxels[x] = new float[resolution][];
 			for (int y = 0; y < resolution; y++) {
+				voxels[x][y] = new float[resolution];
 				for (int z = 0; z < resolution; z++) {
-					_matrices[x + y * resolution + z * resolution * resolution] = Matrix4x4.TRS(GetVoxelPosition(x, y, z), Quaternion.identity, Vector3.one * scale);
+					voxels[x][y][z] = Vector3.Distance(new Vector3(x, y, z), new Vector3(resolution/2f - 0.5f, resolution/2f - 0.5f, resolution/2f - 0.5f)) / resolution;
 				}
 			}
 		}
@@ -44,6 +54,23 @@ public class VoxelGrid : MonoBehaviour {
 	}
 
 	private void Update() {
+		for (int x = 0; x < resolution; x++) {
+			for (int y = 0; y < resolution; y++) {
+				for (int z = 0; z < resolution; z++) {
+					_matrices[x + y * resolution + z * resolution * resolution] = Matrix4x4.TRS(GetVoxelPosition(x, y, z), Quaternion.identity, Vector3.one * scale);
+					Vector4 color;
+					if (voxels[x][y][z] < threshold) {
+						color = _gradient.Evaluate(0);
+					}
+					else {
+						color = _gradient.Evaluate(1);
+					}
+
+					_matrices[x + y * resolution + z * resolution * resolution].SetRow(3, color);
+				}
+			}
+		}
+		
 		var bounds = new Bounds(transform.position, Vector3.one * resolution);
 		_matricesBuffer.SetData(_matrices);
 		_propertyBlock.SetBuffer(matricesId, _matricesBuffer);
--- Assets/Voxel/VoxelShader.shader
+++ Assets/Voxel/VoxelShader.shader
@@ -8,29 +8,32 @@ Shader "Voxel/Procedural Voxel"
     SubShader
     {
         CGPROGRAM
-        #pragma surface ConfigureSurface Standard fullforwardshadows addshadow
+        #pragma surface ConfigureSurface Standard fullforwardshadows
         #pragma instancing_options assumeuniformscaling procedural:ConfigureProcedural
         #pragma editor_sync_compilation
         #pragma target 4.5
 
+        float4 _BaseColor;
+        float _Smoothness;
+        
         #if defined(UNITY_PROCEDURAL_INSTANCING_ENABLED)
             StructuredBuffer<float4x4> _Matrices;
         #endif
-
         void ConfigureProcedural()
         {
             #if defined(UNITY_PROCEDURAL_INSTANCING_ENABLED)
+                _BaseColor = _Matrices[unity_InstanceID]._m30_m31_m32_m33;
                 unity_ObjectToWorld = _Matrices[unity_InstanceID];
+                unity_ObjectToWorld._m30_m31_m32_m33 = float4(0, 0, 0, 1);
             #endif
         }
 
-       struct Input
+
+        struct Input
         {
             float3 worldPos;
         };
 
-        float4 _BaseColor;
-        float _Smoothness;
 
         void ConfigureSurface(Input input, inout SurfaceOutputStandard surface)
         {
